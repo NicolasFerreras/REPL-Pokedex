563a4088c761b3007069815e57af30b92b4ea4f5
NicolasFerreras <nicolasagustinferreras@gmail.com>
2026-07-24 18:32:40 -0300

    feat(model): add LocationArea and Config models

--- internal/model/endpoints.go
+++ internal/model/endpoints.go
@@ -1,6 +1,13 @@
 package model
 
 type Config struct {
-	PreviousURL string
-	NextURL     string
+	PreviousURL *string
+	NextURL     *string
+	BaseURL     string
+}
+
+var ConfigData = Config{
+	PreviousURL: nil,
+	NextURL:     nil,
+	BaseURL:     "https://pokeapi.co/api/v2/location-area/",
 }
